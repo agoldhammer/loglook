7fca9f3e11c36b094d9a85bc9c66d7611c2d2fa1
Art Goldhammer <art.goldhammer@gmail.com>
2023-11-26 12:42:19 -0500

    storing code and bytes as ints rather than strings

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ struct LogEntry {
     ip: String,
     time: String,
     method: String,
-    code: String,
-    bytes: String,
+    code: u32,
+    bytes: u32,
     misc: String,
     ua: String,
 }
@@ -33,18 +33,16 @@ fn get_re_match_part(caps: &Captures<'_>, part_name: &str) -> String {
 }
 
 // Convert line to logentry
-fn make_logentry(line: String) {
-    let re = Regex::new(
-                    r#"(?<ip>\S+) - - \[(?<time>.+)\] (?<method>".+")(?<code>\W\d+\W)(?<bytes>\d+) "(?<misc>.+)" "(?<ua>.+)""#,
-                )
-                .unwrap();
+fn make_logentry(re: &Regex, line: String) {
     let caps = re.captures(&line).unwrap();
+    let code_str = get_re_match_part(&caps, "code");
+    let bytes_str = get_re_match_part(&caps, "bytes");
     let logentry = LogEntry {
         ip: get_re_match_part(&caps, "ip"),
         time: get_re_match_part(&caps, "time"),
         method: get_re_match_part(&caps, "method"),
-        code: get_re_match_part(&caps, "code"),
-        bytes: get_re_match_part(&caps, "bytes"),
+        code: code_str.parse().unwrap(),
+        bytes: bytes_str.parse().unwrap(),
         misc: get_re_match_part(&caps, "misc"),
         ua: get_re_match_part(&caps, "ua"),
     };
@@ -55,32 +53,17 @@ fn make_logentry(line: String) {
 }
 
 fn main() {
+    // regex for parsing nginx log lines in default setup for loal server
+    let re = Regex::new(
+                    r#"(?<ip>\S+) - - \[(?<time>.+)\] (?<method>".+") (?<code>\d+) (?<bytes>\d+) "(?<misc>.+)" "(?<ua>.+)""#,
+                )
+                .unwrap();
     // File hosts.txt must exist in the current path
     if let Ok(lines) = read_lines("./access.log") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(line) = line {
-                make_logentry(line);
-                // let re = Regex::new(
-                //     r#"(?<ip>\d+\.\d+\.\d+\.\d+)\W-\W-\W\[(?<time>.+)\] (?<method>".+")(?<code>\W\d+\W)(?<bytes>\d+) "(?<misc>.+)" "(?<ua>.+)""#,
-                // )
-                // let re = Regex::new(
-                //     r#"(?<ip>\S+) - - \[(?<time>.+)\] (?<method>".+")(?<code>\W\d+\W)(?<bytes>\d+) "(?<misc>.+)" "(?<ua>.+)""#,
-                // )
-                // .unwrap();
-                // let m = re.find(&line).unwrap();
-                // let caps = re.captures(&line).unwrap();
-                // println!("\n");
-                // println!("IP: {}", caps.name("ip").unwrap().as_str());
-                // println!("Time: {}", caps.name("time").unwrap().as_str());
-                // println!("Method: {}", caps.name("method").unwrap().as_str());
-                // println!("Code: {}", caps.name("code").unwrap().as_str());
-                // println!("Bytes: {}", caps.name("bytes").unwrap().as_str());
-                // println!("Misc: {}", caps.name("misc").unwrap().as_str());
-                // println!("UA: {}", caps.name("ua").unwrap().as_str());
-                // println!("m: {}", m.as_str());
-                // println!("line: {}", line);
-                // println!("\n");
+                make_logentry(&re, line);
             }
         }
     } else {
