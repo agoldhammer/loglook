aab1dd905a4ff81be04dcd8841bf9a98a036507c
Art Goldhammer <art.goldhammer@gmail.com>
2023-12-18 09:46:56 -0500

    added TryFrom to LogEntry

--- src/log_entries.rs
+++ src/log_entries.rs
@@ -1,5 +1,9 @@
 // LogEntry holds info derived from one line of log file
+use chrono::DateTime;
 use console::style;
+// use serde::de::Error;
+use core::convert::TryFrom;
+use regex::{Captures, Regex};
 use std::fmt;
 use std::net::IpAddr;
 
@@ -53,5 +57,65 @@ impl fmt::Display for LogEntry {
         write!(f, "  user agent: {}\n", self.ua)?;
         write!(f, "  logged: {}:\n", self.line)?;
         write!(f, "end\n")
+    }
+}
+
+fn get_re_match_part(caps: &Captures<'_>, part_name: &str) -> String {
+    let part = caps.name(part_name).unwrap().as_str();
+    return String::from(part);
+}
+
+impl TryFrom<&String> for LogEntry {
+    type Error = String;
+
+    fn try_from(line: &String) -> Result<Self, Self::Error> {
+        let re = Regex::new(
+                    r#"(?<ip>\S+) \S+ \S+ \[(?<time>.+)\] "(?<method>.*)" (?<code>\d+) (?<nbytes>\d+) "(?<referrer>.*)" "(?<ua>.*)""#,
+                )
+                .unwrap();
+        let caps = match re.captures(line) {
+            Some(x) => x,
+            None => {
+                let s = format!("Failed to parse line: {:?}", line);
+                return Err(s.clone());
+            }
+        };
+        let ip_str = get_re_match_part(&caps, "ip");
+        let ip = ip_str.parse::<IpAddr>().expect("should have good ip addr");
+        let code_str = get_re_match_part(&caps, "code");
+        let nbytes_str = get_re_match_part(&caps, "nbytes");
+        let time_str = get_re_match_part(&caps, "time");
+        let time = DateTime::parse_from_str(time_str.as_str(), "%d/%b/%Y:%H:%M:%S %z")
+            .expect("should be valid time fmt");
+        let le = LogEntry {
+            ip: ip,
+            time: time.to_string(),
+            method: get_re_match_part(&caps, "method"),
+            code: code_str.parse().unwrap(),
+            nbytes: nbytes_str.parse().unwrap(),
+            referrer: get_re_match_part(&caps, "referrer"),
+            ua: get_re_match_part(&caps, "ua"),
+            line: line.to_owned(),
+        };
+        Ok(le)
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn logline_to_logent_conv_test() {
+        let line = "180.149.125.164 - - [25/Nov/2023:00:16:58 -0500] \"GET /stalker_portal/server/tools/auth_simple.php HTTP/1.1\" 404 209 \"-\" \"Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36\"".to_string();
+        let le = LogEntry::try_from(&line).unwrap();
+        assert_eq!(le.line, line);
+    }
+
+    #[test]
+    fn detect_bad_line_test() {
+        let line = "180.149.125.164 - - 25/Nov/2023:00:16:58 -0500] \"GET /stalker_portal/server/tools/auth_simple.php HTTP/1.1\" 404 209 \"-\" \"Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36\"".to_string();
+        assert!(LogEntry::try_from(&line).is_err());
     }
 }
