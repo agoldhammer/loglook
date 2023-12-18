61efcafa10401e6e04dc69efad0a4b6ec00a1d93
Art Goldhammer <art.goldhammer@gmail.com>
2023-12-18 13:11:51 -0500

    cleaned up main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::Parser;
 use std::process;
-// use tokio;
 
 #[derive(Parser)]
 struct Cli {
@@ -8,15 +7,9 @@ struct Cli {
 }
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
-// #[tokio::main]
 async fn main() {
-    // let ip_str = "162.243.141.14";
-    // let ip = ip_str.parse::<IpAddr>().expect("should have good ip addr");
-    // let host = lookup_addr(&ip).unwrap();
-    // println!("host: {}", host);
     let args = Cli::parse();
     println!("Opening file: {:?}", args.path);
-    // process_logfile(&args.path);
     let result = loglook::run(&args.path).await;
     match result {
         Ok(()) => process::exit(0),
