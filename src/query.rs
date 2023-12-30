07fbfb9023634ddd28e246556a61b6e32182782f
Art Goldhammer <art.goldhammer@gmail.com>
2023-12-30 12:11:35 -0500

    expanded query

--- src/query.rs
+++ src/query.rs
@@ -11,7 +11,7 @@ use mongodb::{Collection, Cursor};
 async fn query_date_range(
     coll: Collection<LogEntry>,
 ) -> Result<Cursor<LogEntry>, mongodb::error::Error> {
-    let ct_time: chrono::DateTime<chrono::Utc> = "2023-12-29T00:00:00Z".parse().unwrap();
+    let ct_time: chrono::DateTime<chrono::Utc> = "2023-12-30T18:00:00Z".parse().unwrap();
     let t: bson::DateTime = ct_time.into();
     let filter = doc! {"time": {"$gt": t}};
     coll.find(filter, None).await
@@ -22,7 +22,7 @@ pub async fn find_yesterday(coll: Collection<LogEntry>) {
     let mut cursor = query_date_range(coll).await.unwrap(); //.map_err(anyhow::Error::msg);
     while let Some(maybe_le) = cursor.next().await {
         match maybe_le {
-            Ok(maybe_le) => println!("ip time from lkup: {} {}", maybe_le.ip, maybe_le.time),
+            Ok(maybe_le) => println!("yester: {}\n{}", maybe_le.ip, maybe_le),
             Err(e) => eprintln!("{e}"),
         }
     }
