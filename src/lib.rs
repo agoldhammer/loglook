043f942cf31ae4afd3690dcb4e7cfc2607a45144
Art Goldhammer <art.goldhammer@gmail.com>
2024-01-11 12:27:37 -0500

    printing org preamble on org output

--- src/lib.rs
+++ src/lib.rs
@@ -458,6 +458,11 @@ pub async fn search(
                 let orgs_with_ips = query::get_current_ips_by_org(&current_logentries_coll).await?;
                 let re = Regex::new(org)?;
                 for org_with_ips in orgs_with_ips {
+                    println!(
+                        "{}: {}\n----------",
+                        style("Organization").red(),
+                        style(&org_with_ips.org).yellow()
+                    );
                     if re.is_match(&org_with_ips.org) {
                         let mut ips = org_with_ips.ips;
                         output_ips(
