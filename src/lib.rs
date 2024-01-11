7acc749e3c9c9f8f5b9a1f12f894921b20173ff9
Art Goldhammer <art.goldhammer@gmail.com>
2024-01-11 15:17:51 -0500

    minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -134,13 +134,11 @@ async fn setup_db(db_uri: &str) -> anyhow::Result<(mongodb::Database, HostDataCo
         .build();
     #[allow(unused_variables)]
     let hd_index = host_data_coll.create_index(hd_index_model, None).await?;
-    // ! The index below does not work
     let hd_country_index_model = IndexModel::builder()
         .keys(doc! {"geodata.country": 1})
         .options(None)
         .build();
-    #[allow(unused_variables)]
-    let hd_country_index = host_data_coll
+    host_data_coll
         .create_index(hd_country_index_model, None)
         .await?;
     // * Indices on LogEntry collection
